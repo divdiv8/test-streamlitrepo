81447aa2bac45f009f648eab694759f65b7c5c5f
Divya <36568016+divdiv8@users.noreply.github.com>
2023-05-01 01:33:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,12 @@ st.write('experimenting with this app as we learn streamlit')
 with st.spinner('Wait for it...'):    
   time.sleep(10)
 
+  
+st.sidebar.header(‘huawei apps store：’)
+st.sidebar.subheader(‘1.please chose which app you want to operate’)
+
 side = st.sidebar('side')
 sidecont = st.container()
 sidecont.write('menu')
 with side:
   sidecont(greeting="hello")
-
