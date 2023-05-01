e79b12346c8921975def40f0b9eb24044585dbe7
Divya <36568016+divdiv8@users.noreply.github.com>
2023-05-01 02:05:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,21 +17,21 @@ with st.spinner('Wait for it...'):     #to wait while loading animation
 cont = st.container() #create a container
 cont.write('menu') #write inside container
 with st.sidebar: #to write inside the sidebar
-  choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning", "Python e-Course", "Contact"],
-                         icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
-                         menu_icon="app-indicator", default_index=0,
-                         styles={
-        "container": {"padding": "5!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "#02ab21"},
-    }
-    )
-#   st.header("huawei apps store：")
-#   st.subheader("1.please chose which app you want to operate")
-#   st.radio('select one:',[1,2])
-  
-col1, col2 = st.columns(2) #creating columns
+
+  st.header("huawei apps store：")
+  st.subheader("1.please chose which app you want to operate")
+  st.radio('select one:',[1,2])
+
+col1, col2,col3,col4,col5,col6 = st.columns(2) #creating columns
 col1.write("This is column 1")
 col2.write("This is column 2")
+col2.write("This is column 2")
+col2.write("This is column 2")
+col3.write("This is column 2")
+col4.write("This is column 2")
+col5.write("This is column 2")
+col6.write("This is column 2")
+col2.write("This is column 2")
+col2.write("This is column 2")
+
   
