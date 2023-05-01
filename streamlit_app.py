6c34711d31c1dafe84f844f799c2de5f86996ca4
Divya <36568016+divdiv8@users.noreply.github.com>
2023-05-01 02:08:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import time
 
 
-st.title('🎈 Divya\'s test app')
+st.title('🎈 BTS')
 
 st.write('Hello world!')
 st.write('experimenting with this app as we learn streamlit')
@@ -15,7 +15,8 @@ with st.spinner('Wait for it...'):     #to wait while loading animation
 
 # side = st.sidebar('side')
 cont = st.container() #create a container
-cont.write('menu') #write inside container
+cont.write("BTS debuted in 2013 under Big Hit Entertainment with the single album 2 Cool 4 Skool. BTS released their first Korean and Japanese-language studio albums, Dark & Wild and Wake Up respectively, in 2014. The group's second Korean studio album, Wings (2016), was their first to sell one million copies in South Korea. By 2017, BTS had crossed into the global music market and led the Korean Wave into the United States, becoming the first Korean ensemble to receive a Gold certification from the Recording Industry Association of America (RIAA) for their single \"Mic Drop\", as well as the first act from South Korea to top the Billboard 200 with their studio album Love Yourself: Tear (2018). In 2020, BTS became one of the few groups since the Beatles (in 1966–1968) to chart four US number-one albums in less than two years, with Love Yourself: Answer (2018) becoming the first Korean album certified Platinum by the RIAA; in the same year, they also became the first all-South Korean act to reach number one on both the Billboard Hot 100 and Billboard Global 200 with their Grammy-nominated single \"Dynamite\". Follow-up releases \"Savage Love\", \"Life Goes On\", \"Butter\", and \"Permission to Dance\" made them the fastest act to earn four US number-one singles since Justin Timberlake in 2006.As of 2022, BTS is the best-") 
+#write inside container
 with st.sidebar: #to write inside the sidebar
 
   st.header("huawei apps store：")
