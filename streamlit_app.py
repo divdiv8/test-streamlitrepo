0f77bf09734dd97cc2fcdfa0e54d2a5529e28263
Divya <36568016+divdiv8@users.noreply.github.com>
2023-05-01 01:06:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,6 @@ st.title('🎈 Divya\'s test app')
 st.write('Hello world!')
 st.write('experimenting with this app as we learn streamlit')
 st.balloons()
-st.progress(10) with st.spinner('Wait for it...'):    time.sleep(10)
+st.progress(10) 
+with st.spinner('Wait for it...'):    
+  time.sleep(10)
