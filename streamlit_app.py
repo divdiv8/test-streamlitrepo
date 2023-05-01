d56d78f84a8fb22fdb39f6f1e0c8915135c11714
Divya <36568016+divdiv8@users.noreply.github.com>
2023-05-01 01:31:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,13 @@ st.title('🎈 Divya\'s test app')
 
 st.write('Hello world!')
 st.write('experimenting with this app as we learn streamlit')
-st.balloons()
-
 with st.spinner('Wait for it...'):    
   time.sleep(10)
-sidecont = st.container
-st.sidebar().title('this is inside the sidebar')
+
+st.sidebar('side').title('this is inside the sidebar')
+
+sidecont = st.container()
+
 with st.sidebar:
   sidecont(greeting="hello")
 sidecont.write('menu')
