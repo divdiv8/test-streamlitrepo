3475c34186c0d20d657ac5476069f640b4eefaac
Divya <36568016+divdiv8@users.noreply.github.com>
2023-05-02 16:59:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,4 +37,18 @@ col6.write("This is column 2")
 col2.write("This is column 2")
 col2.write("This is column 2")
 
-  
+import streamlit as st
+
+# Create a list of the navigation bar items
+nav_items = ["Home", "About", "Contact"]
+
+# Create a top navigation bar
+st.markdown("<nav class='navbar navbar-expand-sm navbar-light bg-light'>", unsafe_allow_html=True)
+st.markdown("<a class='navbar-brand' href='/'>My App</a>", unsafe_allow_html=True)
+st.markdown("<ul class='navbar-nav mr-auto'>", unsafe_allow_html=True)
+for nav_item in nav_items:
+  st.markdown("<li class='nav-item'><a class='nav-link' href='{}'>{}</a></li>".format(nav_item, nav_item), unsafe_allow_html=True)
+st.markdown("</ul>", unsafe_allow_html=True)
+st.markdown("</nav>", unsafe_allow_html=True)
+
+
